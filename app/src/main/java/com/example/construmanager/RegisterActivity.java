0805fe1fb77b7234eaeec3b074651f725f79b6a9
DanielRHades <110936904+DanielRHades@users.noreply.github.com>
2023-10-24 00:47:26 -0500

    Avance_Firebase_MoreData

--- app/src/main/java/com/example/construmanager/RegisterActivity.java
+++ app/src/main/java/com/example/construmanager/RegisterActivity.java
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
     private TextInputEditText tfEmail,tfName,tfLastName,tfJob,tfPassword,tfConfirmPassword;
     private Button btnLogin;
     private TextView tvGoLogin;
     private FirebaseAuth mAuth;
+    private FirebaseDatabase fireDB;
+    private DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +38,16 @@ public class RegisterActivity extends AppCompatActivity {
         tfConfirmPassword=findViewById(R.id.tf_confirm_password);
         btnLogin=findViewById(R.id.btn_login);
         tvGoLogin=findViewById(R.id.tv_go_login);
+        fireDB = FirebaseDatabase.getInstance();
+        reference = fireDB.getReference();
+
         tvGoLogin.setOnClickListener(v -> {
             // Lo manda a la pantalla de inicio de sesión al interactuar con el texto resaltado
             Intent myIntent = new Intent(RegisterActivity.this, LoginActivity.class);
             startActivity(myIntent);
             finish();
         });
+
         btnLogin.setOnClickListener(v -> {
             String email,password,cPassword;
             email= String.valueOf(tfEmail.getText());
@@ -51,7 +63,7 @@ public class RegisterActivity extends AppCompatActivity {
                 Toast.makeText(RegisterActivity.this,"Ingresa una contraseña válida", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if(cPassword!=password){
+            if(cPassword.equals(password) == false){
                 Toast.makeText(RegisterActivity.this,"Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
                 return;
             }
@@ -59,18 +71,32 @@ public class RegisterActivity extends AppCompatActivity {
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
+
+                            String getName = tfName.getText().toString();
+                            String getLastName = tfLastName.getText().toString();
+                            String getJob = tfJob.getText().toString();
+                            String getEmail = tfEmail.getText().toString();
+
+                            HashMap<String,Object> hashMap = new HashMap<>();
+
+                            hashMap.put("job",getJob);
+                            hashMap.put("email",getEmail);
+                            hashMap.put("last_name",getLastName);
+                            hashMap.put("name",getName);
+
+                            reference.child("Users").child(getName).setValue(hashMap);
+
                             // Si la cuenta se crea correctamente manda al usuario a main
                             Toast.makeText(RegisterActivity.this, "Cuenta creada.", Toast.LENGTH_SHORT).show();
                             Intent myIntent = new Intent(RegisterActivity.this, MainActivity.class);
                             startActivity(myIntent);
                             finish();
+
                         } else {
                             // De lo contrario solo indica que hubo un problema
                             Toast.makeText(RegisterActivity.this, "Hubo un problema.", Toast.LENGTH_SHORT).show();
                         }
                     });
-
         });
-
     }
 }
