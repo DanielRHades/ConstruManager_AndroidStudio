8c2a8431562cedb2dda56d7b155de56e1a418a42
Hannd15 <lejhernandez08@gmail.com>
2023-11-03 14:41:55 -0500

    Popups

--- app/src/main/java/com/example/construmanager/AddMaterialActivity.java
+++ app/src/main/java/com/example/construmanager/AddMaterialActivity.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputLayout;
